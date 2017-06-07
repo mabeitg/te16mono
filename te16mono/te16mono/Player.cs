671c9776f803a22a1db00b11763965153384d16c
Martin Bergman <martin.bergman4@learnet.se>
2017-06-07 08:34:42 +0200

    First!

---
+++ te16mono/te16mono/Player.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace te16mono
+{
+
+    class Player
+    {
+        public Texture2D texture;
+
+        public void Initialize()
+        {
+            //Initiera värden
+        }
+
+        public void Update()
+        {
+            //Spellogik
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            //Rita på skärmen med spriteBatch
+        }
+    }
+
+}
